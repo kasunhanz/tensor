dd5a97b26e038ff991f97021c1eb105c13938f6e
Gamunu Balagalla <gamunu.balagalla@outlook.com>
2016-06-16 18:58:23 +0530

    remove api from the url path because the frontend is sperated from api

--- api/router.go
+++ api/router.go
@@ -11,87 +11,89 @@ import (
 
 // Route declare all routes
 func Route(r *gin.Engine) {
-	r.GET("/api/ping", func(c *gin.Context) {
+	r.GET("/ping", func(c *gin.Context) {
 		c.String(200, "PONG")
 	})
 
 	// set up the namespace
-	api := r.Group("/api")
+	//future refrence: api := r.Group("/api")
 
-	func(api *gin.RouterGroup) {
-		api.POST("/login", login)
-		api.POST("/logout", logout)
-	}(api.Group("/auth"))
+	auth := r.Group("/auth")
+	{
+		auth.POST("/login", login)
+		auth.POST("/logout", logout)
+	}
 
-	api.Use(authentication)
+	r.Use(authentication)
 
-	api.GET("/ws", sockets.Handler)
+	r.GET("/ws", sockets.Handler)
 
-	api.GET("/info", getSystemInfo)
-	api.GET("/upgrade", checkUpgrade)
-	api.POST("/upgrade", doUpgrade)
+	r.GET("/info", getSystemInfo)
+	r.GET("/upgrade", checkUpgrade)
+	r.POST("/upgrade", doUpgrade)
 
-	func(api *gin.RouterGroup) {
-		api.GET("", getUser)
+	user := r.Group("/user")
+	{
+		user.GET("", getUser)
 		// api.PUT("/user", misc.UpdateUser)
+		user.GET("/tokens", getAPITokens)
+		user.POST("/tokens", createAPIToken)
+		user.DELETE("/tokens/:token_id", expireAPIToken)
+	}
 
-		api.GET("/tokens", getAPITokens)
-		api.POST("/tokens", createAPIToken)
-		api.DELETE("/tokens/:token_id", expireAPIToken)
-	}(api.Group("/user"))
+	r.GET("/projects", projects.GetProjects)
+	r.POST("/projects", projects.AddProject)
+	r.GET("/events", getEvents)
 
-	api.GET("/projects", projects.GetProjects)
-	api.POST("/projects", projects.AddProject)
-	api.GET("/events", getEvents)
+	r.GET("/users", getUsers)
+	r.POST("/users", addUser)
+	r.GET("/users/:user_id", getUserMiddleware, getUser)
+	r.PUT("/users/:user_id", getUserMiddleware, updateUser)
+	r.POST("/users/:user_id/password", getUserMiddleware, updateUserPassword)
+	r.DELETE("/users/:user_id", getUserMiddleware, deleteUser)
 
-	api.GET("/users", getUsers)
-	api.POST("/users", addUser)
-	api.GET("/users/:user_id", getUserMiddleware, getUser)
-	api.PUT("/users/:user_id", getUserMiddleware, updateUser)
-	api.POST("/users/:user_id/password", getUserMiddleware, updateUserPassword)
-	api.DELETE("/users/:user_id", getUserMiddleware, deleteUser)
+	apiProject := r.Group("/project/:project_id")
+	{
+		apiProject.Use(projects.ProjectMiddleware)
 
-	func(api *gin.RouterGroup) {
-		api.Use(projects.ProjectMiddleware)
+		apiProject.GET("", projects.GetProject)
 
-		api.GET("", projects.GetProject)
+		apiProject.GET("/events", getEvents)
 
-		api.GET("/events", getEvents)
+		apiProject.GET("/users", projects.GetUsers)
+		apiProject.POST("/users", projects.AddUser)
+		apiProject.POST("/users/:user_id/admin", projects.UserMiddleware, projects.MakeUserAdmin)
+		apiProject.DELETE("/users/:user_id/admin", projects.UserMiddleware, projects.MakeUserAdmin)
+		apiProject.DELETE("/users/:user_id", projects.UserMiddleware, projects.RemoveUser)
 
-		api.GET("/users", projects.GetUsers)
-		api.POST("/users", projects.AddUser)
-		api.POST("/users/:user_id/admin", projects.UserMiddleware, projects.MakeUserAdmin)
-		api.DELETE("/users/:user_id/admin", projects.UserMiddleware, projects.MakeUserAdmin)
-		api.DELETE("/users/:user_id", projects.UserMiddleware, projects.RemoveUser)
+		apiProject.GET("/keys", projects.GetKeys)
+		apiProject.POST("/keys", projects.AddKey)
+		apiProject.PUT("/keys/:key_id", projects.KeyMiddleware, projects.UpdateKey)
+		apiProject.DELETE("/keys/:key_id", projects.KeyMiddleware, projects.RemoveKey)
 
-		api.GET("/keys", projects.GetKeys)
-		api.POST("/keys", projects.AddKey)
-		api.PUT("/keys/:key_id", projects.KeyMiddleware, projects.UpdateKey)
-		api.DELETE("/keys/:key_id", projects.KeyMiddleware, projects.RemoveKey)
+		apiProject.GET("/repositories", projects.GetRepositories)
+		apiProject.POST("/repositories", projects.AddRepository)
+		apiProject.DELETE("/repositories/:repository_id", projects.RepositoryMiddleware, projects.RemoveRepository)
 
-		api.GET("/repositories", projects.GetRepositories)
-		api.POST("/repositories", projects.AddRepository)
-		api.DELETE("/repositories/:repository_id", projects.RepositoryMiddleware, projects.RemoveRepository)
+		apiProject.GET("/inventory", projects.GetInventory)
+		apiProject.POST("/inventory", projects.AddInventory)
+		apiProject.PUT("/inventory/:inventory_id", projects.InventoryMiddleware, projects.UpdateInventory)
+		apiProject.DELETE("/inventory/:inventory_id", projects.InventoryMiddleware, projects.RemoveInventory)
 
-		api.GET("/inventory", projects.GetInventory)
-		api.POST("/inventory", projects.AddInventory)
-		api.PUT("/inventory/:inventory_id", projects.InventoryMiddleware, projects.UpdateInventory)
-		api.DELETE("/inventory/:inventory_id", projects.InventoryMiddleware, projects.RemoveInventory)
+		apiProject.GET("/environment", projects.GetEnvironment)
+		apiProject.POST("/environment", projects.AddEnvironment)
+		apiProject.PUT("/environment/:environment_id", projects.EnvironmentMiddleware, projects.UpdateEnvironment)
+		apiProject.DELETE("/environment/:environment_id", projects.EnvironmentMiddleware, projects.RemoveEnvironment)
 
-		api.GET("/environment", projects.GetEnvironment)
-		api.POST("/environment", projects.AddEnvironment)
-		api.PUT("/environment/:environment_id", projects.EnvironmentMiddleware, projects.UpdateEnvironment)
-		api.DELETE("/environment/:environment_id", projects.EnvironmentMiddleware, projects.RemoveEnvironment)
+		apiProject.GET("/templates", projects.GetTemplates)
+		apiProject.POST("/templates", projects.AddTemplate)
+		apiProject.PUT("/templates/:template_id", projects.TemplatesMiddleware, projects.UpdateTemplate)
+		apiProject.DELETE("/templates/:template_id", projects.TemplatesMiddleware, projects.RemoveTemplate)
 
-		api.GET("/templates", projects.GetTemplates)
-		api.POST("/templates", projects.AddTemplate)
-		api.PUT("/templates/:template_id", projects.TemplatesMiddleware, projects.UpdateTemplate)
-		api.DELETE("/templates/:template_id", projects.TemplatesMiddleware, projects.RemoveTemplate)
-
-		api.GET("/tasks", tasks.GetAll)
-		api.POST("/tasks", tasks.AddTask)
-		api.GET("/tasks/:task_id/output", tasks.GetTaskMiddleware, tasks.GetTaskOutput)
-	}(api.Group("/project/:project_id"))
+		apiProject.GET("/tasks", tasks.GetAll)
+		apiProject.POST("/tasks", tasks.AddTask)
+		apiProject.GET("/tasks/:task_id/output", tasks.GetTaskMiddleware, tasks.GetTaskOutput)
+	}
 }
 
 func getSystemInfo(c *gin.Context) {
