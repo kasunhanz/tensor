3b0c958dab1a1cb9d6bf4b7748ca0cc56114e018
Gamunu Balagalla <gamunu.balagalla@outlook.com>
2016-07-08 00:17:46 +0530

    Mongo connection and util package update
    Updated mongo connection to work with replica set now it's required to have a replicaset

--- cli/main.go
+++ cli/main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"os"
-	"path"
 	"strings"
 	"time"
 
@@ -19,6 +16,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
 	"pearson.com/hilbert-space/api/addhoctasks"
+	"gopkg.in/mgo.v2/bson"
 )
 
 func main() {
@@ -26,40 +24,18 @@ func main() {
 		os.Exit(doSetup())
 	}
 
-	if util.Upgrade {
-		if err := util.DoUpgrade(util.Version); err != nil {
-			panic(err)
-		}
-
-		os.Exit(0)
-	}
-
 	fmt.Printf("Hilbertspace %v\n", util.Version)
 	fmt.Printf("Port %v\n", util.Config.Port)
-	fmt.Printf("MySQL %v@%v %v\n", util.Config.MySQL.Username, util.Config.MySQL.Hostname, util.Config.MySQL.DbName)
+	fmt.Printf("MongoDB %v@%v %v\n", util.Config.MongoDB.Username, util.Config.MongoDB.Hosts, util.Config.MongoDB.DbName)
 	fmt.Printf("Tmp Path (projects home) %v\n", util.Config.TmpPath)
 
 	if err := database.Connect(); err != nil {
 		panic(err)
 	}
-	if err := database.MdbConnect(); err != nil {
-		panic(err)
-	}
-	models.SetupDBLink()
 
 	defer func() {
-		database.Mysql.Db.Close()
 		database.MongoDb.Session.Close()
 	}()
-
-	if util.Migration {
-		fmt.Println("\n Running DB Migrations")
-		if err := database.MigrateAll(); err != nil {
-			panic(err)
-		}
-
-		return
-	}
 
 	go sockets.StartWS()
 	r := gin.New()
@@ -67,7 +43,6 @@ func main() {
 
 	api.Route(r)
 
-	go checkUpdates()
 	go tasks.StartRunner()
 	go addhoctasks.StartRunner()
 
@@ -75,66 +50,16 @@ func main() {
 }
 
 func recovery(c *gin.Context) {
+
 	//report to bug nofiy system
 	c.Next()
 }
 
 func doSetup() int {
-	fmt.Print(`
- Hello! You will now be guided through a setup to:
-
- 1. Set up configuration for a MySQL/MariaDB database
- 2. Set up a path for your playbooks (auto-created)
- 3. Run database Migrations
- 4. Set up initial seamphore user & password
-
-`)
-
-	var b []byte
-	setup := util.NewConfig()
-	for {
-		setup.Scan()
-		setup.GenerateCookieSecrets()
-
-		var err error
-		b, err = json.MarshalIndent(&setup, " ", "\t")
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Printf("\n Generated configuration:\n %v\n\n", string(b))
-		fmt.Print(" > Is this correct? (yes/no): ")
-
-		var answer string
-		fmt.Scanln(&answer)
-		if answer == "yes" || answer == "y" {
-			break
-		}
-
-		fmt.Println()
-		setup = util.NewConfig()
-	}
-
-	fmt.Printf(" Running: mkdir -p %v..\n", setup.TmpPath)
-	os.MkdirAll(setup.TmpPath, 0755)
-
-	configPath := path.Join(setup.TmpPath, "/hilbertspace_config.json")
-	fmt.Printf(" Configuration written to %v..\n", configPath)
-	if err := ioutil.WriteFile(configPath, b, 0644); err != nil {
-		panic(err)
-	}
-
-	fmt.Println(" Pinging database..")
-	util.Config = setup
+	fmt.Println("Checking database connectivity.. Please be patient.")
 
 	if err := database.Connect(); err != nil {
 		fmt.Printf("\n Cannot connect to database!\n %v\n", err.Error())
-		os.Exit(1)
-	}
-
-	fmt.Println("\n Running DB Migrations..")
-	if err := database.MigrateAll(); err != nil {
-		fmt.Printf("\n Database migrations failed!\n %v\n", err.Error())
 		os.Exit(1)
 	}
 
@@ -147,9 +72,11 @@ func doSetup() int {
 	user.Email = strings.ToLower(user.Email)
 
 	var existingUser models.User
-	database.Mysql.SelectOne(&existingUser, "select * from user where email=? or username=?", user.Email, user.Username)
 
-	if existingUser.ID > 0 {
+	userc := database.MongoDb.C("user")
+	err := userc.Find(bson.M{"email": user.Email, "username": user.Username}).One(&existingUser)
+
+	if err == nil {
 		// user already exists
 		fmt.Printf("\n Welcome back, %v! (a user with this username/email is already set up..)\n\n", existingUser.Name)
 	} else {
@@ -157,16 +84,22 @@ func doSetup() int {
 		user.Password = readNewline(" > Password: ", stdin)
 		pwdHash, _ := bcrypt.GenerateFromPassword([]byte(user.Password), 11)
 
-		if _, err := database.Mysql.Exec("insert into user set name=?, username=?, email=?, password=?, created=NOW()", user.Name, user.Username, user.Email, pwdHash); err != nil {
+		newUser := models.User{
+			ID: bson.NewObjectId(),
+			Name: user.Name,
+			Username: user.Username,
+			Email: user.Email,
+			Password: string(pwdHash),
+			Created:time.Now(),
+		}
+
+		if err := newUser.Insert(); err != nil {
 			fmt.Printf(" Inserting user failed. If you already have a user, you can disregard this error.\n %v\n", err.Error())
 			os.Exit(1)
 		}
 
 		fmt.Printf("\n You are all setup %v!\n", user.Name)
 	}
-
-	fmt.Printf(" Re-launch this program pointing to the configuration file\n\n./hilbertspace -config %v\n\n", configPath)
-	fmt.Printf(" To run as daemon:\n\nnohup ./hilbertspace -config %v &\n\n", configPath)
 	fmt.Printf(" You can login with %v or %v.\n", user.Email, user.Username)
 
 	return 0
@@ -179,14 +112,4 @@ func readNewline(pre string, stdin *bufio.Reader) string {
 	str = strings.Replace(str, "\n", "", -1)
 
 	return str
-}
-
-func checkUpdates() {
-	util.CheckUpdate(util.Version)
-
-	t := time.NewTicker(time.Hour * 24)
-
-	for range t.C {
-		util.CheckUpdate(util.Version)
-	}
 }
